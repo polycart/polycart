d7178b52f833416016d00894daaac908a93808bd
leylee <leylee2001@gmail.com>
2020-08-25 20:54:05 +0800

    add try except block

--- PolyCart/Cart/location.py
+++ PolyCart/Cart/location.py
@@ -6,9 +6,12 @@ from serial import Serial
 class Location:
     serial: Serial = None
     calc = None
+    # anchor_location = ((0.0, 0.0, 0.0),
+    #                    (4.0, 7.8, 0.0),
+    #                    (0.8, 10.2, 0.0))
     anchor_location = ((0.0, 0.0, 0.0),
-                       (4.0, 7.8, 0.0),
-                       (0.8, 10.2, 0.0))
+                       (0.0, 0.8, 0.0),
+                       (0.8, 0.0, 0.0))
     SO_PATH = './trilateration.so'
     SERIAL_PATH = '/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0'
 
@@ -18,41 +21,50 @@ class Location:
         @return: a float tuple (x, y, z) in meter.
             If an error occurred, return None.
         """
-        if cls.serial is None:
-            cls.serial = Serial(cls.SERIAL_PATH, 115200)
-        else:
-            cls.serial.close()
-            cls.serial.open()
+        try:
+            if cls.serial is None:
+                try:
+                    cls.serial = Serial(cls.SERIAL_PATH, 115200)
+                except Exception as e:
+                    print(e)
+                    return None
+            else:
+                cls.serial.close()
+                cls.serial.open()
 
-        anchor_distance: tuple = cls._get_anchor_distance()
-        if anchor_distance is None:
-            return None
+            anchor_distance: tuple = cls._get_anchor_distance()
+            if anchor_distance is None:
+                return None
 
-        if cls.calc is None:
-            cls.calc = CDLL(cls.SO_PATH)
+            if cls.calc is None:
+                cls.calc = CDLL(cls.SO_PATH)
 
-            class vec3d(Structure):
-                _fields_ = [('x', c_double),
-                            ('y', c_double),
-                            ('z', c_double)]
+                class vec3d(Structure):
+                    _fields_ = [('x', c_double),
+                                ('y', c_double),
+                                ('z', c_double)]
 
-            cls.vec3d = vec3d
-            cls.vec3d4 = vec3d * 4
-            cls.int4 = c_int * 4
-            cls.anchorArray = cls.vec3d4()
+                cls.vec3d = vec3d
+                cls.vec3d4 = vec3d * 4
+                cls.int4 = c_int * 4
+                cls.anchorArray = cls.vec3d4()
+                for i in range(3):
+                    cls.anchorArray[i].x = cls.anchor_location[i][0]
+                    cls.anchorArray[i].y = cls.anchor_location[i][1]
+                    cls.anchorArray[i].z = cls.anchor_location[i][2]
+                cls.report = cls.vec3d()
+                cls.Range_deca = cls.int4()
+
             for i in range(3):
-                cls.anchorArray[i].x = cls.anchor_location[i][0]
-                cls.anchorArray[i].y = cls.anchor_location[i][1]
-                cls.anchorArray[i].z = cls.anchor_location[i][2]
-            cls.report = cls.vec3d()
-            cls.Range_deca = cls.int4()
-
-        for i in range(3):
-            cls.Range_deca[i] = anchor_distance[i]
-        result = cls.calc.GetLocation(byref(cls.report), 0, cls.anchorArray, cls.Range_deca)
-        if result == -1:
+                cls.Range_deca[i] = anchor_distance[i]
+            result = cls.calc.GetLocation(byref(cls.report), 0, cls.anchorArray, cls.Range_deca)
+            if result == -1:
+                print('result == -1')
+                return None
+            return cls.report.x, cls.report.y, cls.report.z
+        except Exception as e:
+            print(e)
             return None
-        return cls.report.x, cls.report.y, cls.report.z
 
     @classmethod
     def _get_anchor_distance(cls):
@@ -67,7 +79,7 @@ class Location:
         '''The “mr” message consists of tag to anchor raw ranges, “mc” tag to anchor range bias corrected
         ranges – used for tag location and “ma” anchor to anchor range bias corrected ranges – used for
         anchor auto-positioning.
-        
+
         MID MASK RANGE0 RANGE1 RANGE2 RANGE3 NRANGES RSEQ DEBUG aT:A
         MID this is the message ID, as described above: mr, mc and ma
         MASK this states which RANGEs are valid, if MASK=7 then only RANGE0, RANGE1 and RANGE2
@@ -88,6 +100,7 @@ class Location:
         entry_items = mc_messages.split()
         mask = int(entry_items[1], 16)
         if mask != 7:
+            print('mask != 7')
             return None
         x = int(entry_items[2], 16)
         y = int(entry_items[3], 16)
